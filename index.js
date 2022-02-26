d012008b75504d1731b0472b2815dd85dcc4a46c
Patrick Young <patrickyoung281@gmail.com>
2022-02-26 14:39:41 -0500

    commit tree 930ff2946221420115b0606ec3c7aeff014d859a

---
+++ index.js
@@ -0,0 +1,113 @@
+var a = random()*1000
+var b = random()*1000
+
+function add(a, b) {
+    return a +b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function increment (a) {
+    return a +1;
+}
+
+function decrement (a) {
+    return a-1;
+}
+
+function makeInt (n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal (n) {
+    return parseFloat(n);
+}
+
+console.log(makeInt("10.4"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const a = random()*1000;
+const b = random()*1000;
+const n = 10;
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function increment (a) {
+    return a + 1; 
+}
+
+function decrement (a) {
+    return a -1;
+}
+
+function makeInt (a) {
+    return parseInt (a, 10);
+}
+
+function preserveDecimal (a) {
+    return parseFloat (a);
+}
+
+*/
